00f84cb2600532f3524261f56b55b71a44b3035b
csae8092 <p.andorfer@gmail.com>
2025-10-15 08:49:34 +0200

    wip: fulltext search

--- pyscripts/make_ts_index.py
+++ pyscripts/make_ts_index.py
@@ -1,20 +1,27 @@
 import glob
+import os
 
 from acdh_cfts_pyutils import TYPESENSE_CLIENT as client
 from acdh_tei_pyutils.tei import TeiReader
+from acdh_tei_pyutils.utils import check_for_hash, extract_fulltext
 from tqdm import tqdm
 from typesense.api_call import ObjectNotFound
 
-files = glob.glob("./data/editions/*/*.xml")
+files = glob.glob("./data/editions/*.xml")
+tag_blacklist = [
+    "{http://www.tei-c.org/ns/1.0}abbr",
+    "{http://www.tei-c.org/ns/1.0}del",
+]
 
 
 try:
-    client.collections["JP-Briefe"].delete()
+    client.collections["jpbriefe"].delete()
 except ObjectNotFound:
     pass
 
 current_schema = {
-    "name": "JP-Briefe",
+    "name": "jpbriefe",
+    "enable_nested_fields": True,
     "fields": [
         {"name": "id", "type": "string"},
         {"name": "rec_id", "type": "string"},
@@ -26,9 +33,11 @@ current_schema = {
             "optional": True,
             "facet": True,
         },
-        {"name": "persons", "type": "string[]", "facet": True, "optional": True},
-        {"name": "places", "type": "string[]", "facet": True, "optional": True},
-        {"name": "orgs", "type": "string[]", "facet": True, "optional": True},
+        {"name": "sender", "type": "object[]", "facet": True, "optional": True},
+        {"name": "receiver", "type": "object[]", "facet": True, "optional": True},
+        {"name": "persons", "type": "object[]", "facet": True, "optional": True},
+        {"name": "places", "type": "object[]", "facet": True, "optional": True},
+        {"name": "bibls", "type": "object[]", "facet": True, "optional": True},
     ],
 }
 
@@ -39,8 +48,25 @@ records = []
 cfts_records = []
 for x in tqdm(files, total=len(files)):
     doc = TeiReader(x)
+    try:
+        body = doc.any_xpath(".//tei:body")[0]
+    except IndexError:
+        continue
+    record = {}
+    record["id"] = os.path.split(x)[-1].replace(".xml", "")
+    record["rec_id"] = os.path.split(x)[-1].replace(".xml", "")
+    record["title"] = doc.any_xpath(".//tei:title[@n='digital']")[0].text
+    record["full_text"] = extract_fulltext(body, tag_blacklist=tag_blacklist)
+    record["sender"] = []
+    for y in doc.any_xpath('.//tei:correspAction[@type="sent"]/tei:persName'):
+        item = {}
+        item["id"] = check_for_hash(y.attrib["key"])
+        item["label"] = y.text
+        record["sender"].append(item)
+
+    records.append(record)
 
 
-make_index = client.collections["JP-Briefe"].documents.import_(records)
+make_index = client.collections["jpbriefe"].documents.import_(records)
 print(make_index)
-print("done with indexing JP-Briefe")
+print("done with indexing jpbriefe")

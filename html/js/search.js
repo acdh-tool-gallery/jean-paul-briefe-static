c46629004233069550ff943400bf2104f544a02d
Ivana Dobcheva <ivana.dob@gmail.com>
2025-10-16 11:51:46 +0200

    fix #1, search.js, adjust xslt and css, allow sorting in index fields

--- html/js/search.js
+++ html/js/search.js
@@ -1,1 +1,257 @@
-console.log("this is the file to configure the search client")
+const indexName = "jpbriefe";
+
+const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
+  server: {
+    apiKey: "MVu9C76zrLIh42RZtszVmOkDEdsiUPOg",
+    nodes: [
+      {
+        host: "typesense.acdh-dev.oeaw.ac.at",
+        port: "443",
+        protocol: "https",
+      },
+    ],
+    cacheSearchResultsForSeconds: 2 * 60,
+  },
+  additionalSearchParameters: {
+    query_by: "full_text",
+    // sort_by: "rec_id:asc",
+  },
+});
+
+const DEFAULT_CSS_CLASSES = {
+  searchableInput: "form-control form-control-sm m-2 border-light-2",
+  searchableSubmit: "d-none",
+  searchableReset: "d-none",
+  showMore: "btn btn-secondary btn-sm align-content-center",
+  list: "list-unstyled",
+  count: "badge m-2 badge-secondary",
+  label: "d-flex align-items-center text-capitalize",
+  checkbox: "m-2",
+};
+
+const searchClient = typesenseInstantsearchAdapter.searchClient;
+const search = instantsearch({
+  indexName: indexName,
+  searchClient,
+});
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: "#searchbox",
+    autofocus: true,
+    cssClasses: {
+      form: "form-inline",
+      input: "form-control col-md-11",
+      submit: "btn",
+      reset: "btn",
+    },
+  }),
+
+  instantsearch.widgets.hits({
+    container: "#hits",
+    cssClasses: {
+      item: "w-100",
+    },
+    templates: {
+      empty: "Keine Resultate für <q>{{ query }}</q>",
+      item(hit, { html, components }) {
+        return html` <div>
+          <div class="fs-3"><a href="${hit.rec_id}.html" class="custom-link">${hit.title}</a></div>
+          <p>
+            ${hit._snippetResult.full_text.matchedWords.length > 0
+              ? components.Snippet({ hit, attribute: "full_text" })
+              : ""}
+          </p>
+           ${hit.places.map(
+            (item) => html`<a href="${item.id}.html" class="pe-2 custom-link"><i class="bi bi-geo-alt pe-1"></i>${item.label}</a>`
+          )}
+          <br />
+          ${hit.persons.map(
+            (item) => html`<a href="${item.id}.html" class="pe-2 custom-link"><i class="bi bi-person pe-1"></i>${item.label}</a>`
+          )}
+          <br />
+        </div>`;
+      },
+    },
+  }),
+
+  instantsearch.widgets.sortBy({
+    container: "#sort-by",
+    items: [
+      { label: "Standard", value: `${indexName}` },
+      { label: "ID (aufsteigend)", value: `${indexName}/sort/rec_id:asc` },
+      { label: "ID (absteigend)", value: `${indexName}/sort/rec_id:desc` },
+    ],
+  }),
+
+  instantsearch.widgets.stats({
+    container: "#stats-container",
+    templates: {
+      text: `
+          {{#areHitsSorted}}
+            {{#hasNoSortedResults}}Keine Treffer{{/hasNoSortedResults}}
+            {{#hasOneSortedResults}}1 Treffer{{/hasOneSortedResults}}
+            {{#hasManySortedResults}}{{#helpers.formatNumber}}{{nbSortedHits}}{{/helpers.formatNumber}} Treffer {{/hasManySortedResults}}
+            aus {{#helpers.formatNumber}}{{nbHits}}{{/helpers.formatNumber}}
+          {{/areHitsSorted}}
+          {{^areHitsSorted}}
+            {{#hasNoResults}}Keine Treffer{{/hasNoResults}}
+            {{#hasOneResult}}1 Treffer{{/hasOneResult}}
+            {{#hasManyResults}}{{#helpers.formatNumber}}{{nbHits}}{{/helpers.formatNumber}} Treffer{{/hasManyResults}}
+          {{/areHitsSorted}}
+          gefunden in {{processingTimeMS}}ms
+        `,
+    },
+  }),
+
+  instantsearch.widgets.panel({
+    collapsed: ({ state }) => {
+      return state.query.length === 0;
+    },
+    templates: {
+      header: "Absender",
+    },
+  })(instantsearch.widgets.refinementList)({
+    container: "#refinement-list-sender",
+    attribute: "sender.label",
+    searchable: true,
+    showMore: true,
+    showMoreLimit: 50,
+    templates: {
+  showMoreText: ({ isShowingMore }) => 
+    isShowingMore ? 'Weniger anzeigen' : 'Mehr anzeigen'
+},
+    limit: 10,
+    searchablePlaceholder: "Suche nach Absendern",
+    cssClasses: DEFAULT_CSS_CLASSES,
+  }),
+
+  instantsearch.widgets.panel({
+    collapsed: ({ state }) => {
+      return state.query.length === 0;
+    },
+    templates: {
+      header: "Empfänger",
+    },
+  })(instantsearch.widgets.refinementList)({
+    container: "#refinement-list-receiver",
+    attribute: "receiver.label",
+    searchable: true,
+    showMore: true,
+    showMoreLimit: 50,
+    templates: {
+  showMoreText: ({ isShowingMore }) => 
+    isShowingMore ? 'Weniger anzeigen' : 'Mehr anzeigen'
+},
+    limit: 10,
+    searchablePlaceholder: "Suche nach Empfängern",
+    cssClasses: DEFAULT_CSS_CLASSES,    
+  }),
+
+ 
+  instantsearch.widgets.panel({
+    collapsed: ({ state }) => {
+      return state.query.length === 0;
+    },
+    templates: {
+      header: "Personen",
+    },
+  })(instantsearch.widgets.refinementList)({
+    container: "#refinement-list-persons",
+    attribute: "persons.label",
+    searchable: true,
+    showMore: true,
+    showMoreLimit: 50,
+    templates: {
+  showMoreText: ({ isShowingMore }) => 
+    isShowingMore ? 'Weniger anzeigen' : 'Mehr anzeigen'
+},
+    limit: 10,
+    searchablePlaceholder: "Suche nach Personen",
+    cssClasses: DEFAULT_CSS_CLASSES,
+  }),
+
+  instantsearch.widgets.panel({
+    collapsed: ({ state }) => {
+      return state.query.length === 0;
+    },
+    templates: {
+      header: "Orten",
+    },
+  })(instantsearch.widgets.refinementList)({
+    container: "#refinement-list-places",
+    attribute: "places.label",
+    searchable: true,
+    showMore: true,
+    showMoreLimit: 50,
+    limit: 10,
+    searchablePlaceholder: "Suche nach Orten",
+    cssClasses: DEFAULT_CSS_CLASSES,
+    templates: {
+    showMoreText({ isShowingMore }) {
+      return isShowingMore ? 'Weniger anzeigen' : 'Mehr anzeigen';
+    },
+  },
+  }),
+
+  
+  instantsearch.widgets.pagination({
+    container: "#pagination",
+    padding: 2,
+    cssClasses: {
+      list: "pagination",
+      item: "page-item",
+      link: "page-link",
+    },
+  }),
+  instantsearch.widgets.clearRefinements({
+    container: "#clear-refinements",
+    templates: {
+      resetLabel: "Filter zurücksetzen",
+    },
+    cssClasses: {
+      button: "btn",
+    },
+  }),
+
+  instantsearch.widgets.currentRefinements({
+    container: "#current-refinements",
+    cssClasses: {
+      delete: "btn",
+      label: "badge",
+    },
+    transformItems(items) {
+			return items.map((item) => ({
+				...item,
+				label:
+					item.attribute === "sender.label"
+						? "Absender"
+						: item.attribute === "receiver.label"
+							? "Empfänger"
+							: item.attribute === "persons.label"
+								? "Personen"
+								: item.attribute === "places.label"
+									? "Orte"
+                                    : item.label,
+			}));
+		},
+  })
+]);
+
+// Show/hide the Filter panel
+const showFilter = document.querySelector("#filter-button");
+const filters = document.querySelector("#refinements-section");
+if (showFilter) {
+	showFilter.addEventListener("click", function () {
+		filters?.classList.toggle("d-none");
+	});
+}
+
+
+search.addWidgets([
+  instantsearch.widgets.configure({
+    attributesToSnippet: ["full_text"],
+  }),
+]);
+
+search.start();
